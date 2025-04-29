6ce6595495e14c863c025dcbf9df8be42ad8fb49
mrpaulwoods <mr.paul.woods@gmail.com>
2025-04-28 22:56:09 -0500

    feat(backend): enable CORS for AddressController

--- backend/src/main/java/com/mrpaulwoods/quicktravel/backend/controller/AddressController.java
+++ backend/src/main/java/com/mrpaulwoods/quicktravel/backend/controller/AddressController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/addresses")
+@CrossOrigin(origins = "*")
 public class AddressController {
 
     private final AddressRepository addressRepository;
